6a3fa24db62135508d39a50740865698ee1d4cb4
Chad Kreutzer <kanthalion@gmail.com>
2017-01-12 13:49:11 +0000

    parallax fixed and sass implemented

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'redcarpet'
+require 'sass/plugin/rack'
+
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
 
 get '/' do
   erb :index
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-  // $('#story').css('margin-top', -($('#about').height() - 20 ) + 'px')
-  $('.section').affix({
-    offset: {
-      top: $('#home').height()
-    }
-  });
+  
 });
