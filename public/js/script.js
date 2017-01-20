eb888411fbcf413c3e2b65bd21ddffad828c14b9
Chad Kreutzer <kanthalion@gmail.com>
2017-01-20 13:09:38 +0000

    add scrollspy to nav

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $('body').scrollspy({ target: '#navbar' })
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
