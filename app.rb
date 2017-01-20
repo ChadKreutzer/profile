aa930d19bce740c40380e960135818fcb96903c9
Chad Kreutzer <kanthalion@gmail.com>
2017-01-20 13:04:02 +0000

    mostly done with design

--- app.rb
+++ app.rb
@@ -19,15 +19,25 @@ end
 post '/' do
   require 'pony'
   
-  name  = params[:name]
+  # name  = params[:name]
   # mail = params[:mail]
-  subject = params[:subject]
-  body  = params[:body]
+  # subject = params[:subject]
+  # body  = params[:body]
   
-  Pony.options = { subject: "#{subject}",
-                   body: "#{name}\n\n#{body}"
-                  }
-  Pony.mail(to: "chadkreutzer@outlook.com")
+  # Pony.options = { subject: "#{subject}",
+  #                 body: "#{name}\n#{mail}\n\n#{body}",
+  #                 via_options: {
+  #                   address:'smtp.sendgrid.net',
+  #                   port: '587',
+  #                   enable_starttls_auto: true,
+  #                   user_name:'apikey',
+  #                   password: ENV['SENDGRID_API_KEY'],
+  #                   authentication: :plain
+  #                 }
+  #                 }
+  # Pony.mail(to: "chadkreutzer@outlook.com")
+  
+  # Look into figaro gem for .gitignore.
   
   redirect '/#contact'
 end
